44a1cdeeec4469b1c4c5255fb19e33fae0080c65
Tien Pham <r3dsky02@yahoo.com.vn>
2019-04-18 22:30:51 +0300

    Hooks TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+type FormElem = React.FormEvent<HTMLFormElement>
+
 export default function App(): JSX.Element {
-  return <h1>Hello</h1>
+  const [value, setValue] = React.useState<string>("")
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault()
+    setValue("")
+  }
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  )
 }
 
 const root = document.getElementById("app-root")
